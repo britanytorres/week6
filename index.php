556f55c9003c9c4dd9aa70eee959173dab840ec4
Britany Torres <torres.britany@student.greenriver.edu>
2023-02-09 14:21:29 -0500

    added template practice

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
 <?php
 //this is my controller
-//sessions sends "headers already sent" error when ANY white space is on front
-//  of your <?php
 
 //turn on error reporting
 ini_set("display_errors", 1);
 error_reporting(E_ALL);
-
-//start a session
-//session_start();
 
 //require autoload file
 require_once('vendor/autoload.php');
@@ -16,18 +11,27 @@ require_once('vendor/autoload.php');
 //instantiate F# base class
 $f3 = Base::instance();
 
+//define a default route
+$f3->route('GET /', function($f3){
+    //add food and color
+    $f3->set('food', 'pizza');
+    $f3->set('color', 'blue');
+    $f3->set('temp', 67);
+    $f3->set('radius', 10);
 
-//define a default route (328/diner)
-/*function(){
-    $msg = "This is a function";
-    echo $msg;
-}*/
-$f3->route('GET /', function(){
+    //encrypts the password
+    //$f3->set('password', sha1('pizza'));
+
+    //define an array of fruits
+    $fruits = array('apple', 'grapes', 'orange');
+    $f3->set('fruits', $fruits);
+    //$f3->set('fruits', array('apples', 'oranges'))
+
+    $f3->set('colors', array('red', 'orange', 'blue'));
+
     $view = new Template();
     echo $view->render("views/info.html");
 });
-
-//if the form has been submitted
 
 //Run Fat Free
 $f3->run();
